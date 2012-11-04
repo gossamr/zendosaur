d88f1f02fcf099eb7b6d13484a63efa670a9c8c1
Jehan Tremback <jehan.tremback@gmail.com>
2012-11-03 21:44:39 -0700

    basic layout set up and row display figured out

---
+++ javascripts/scihack.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $(".container").click(function(){
+      $(".container").removeClass("selected");
+      $(this).addClass("selected");
+  });
+  
+});
--- scihack.js
+++ scihack.js
@@ -1,17 +1,9 @@
 $(document).ready(function(){
-  function reader() {
-      var propSet = [];
-      $(".container div").each(function() {
-          classes = $(this).attr("class");
-          propSet.push(classes)
-      });
-      return propSet;
-  }
-
   $(".container").click(function(){
       $(".container").removeClass("selected");
       $(this).addClass("selected");
   });
+  
 });
 
 function rnd(num) {
