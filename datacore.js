b23fe498718d47f55d3286b5e0d722639d327bf2
Karenism <karebearinmind@yahoo.com>
2012-11-04 09:46:37 -0800

    added backbone support, basic models for meals & foods, stub code for adding a guess

--- datacore.js
+++ datacore.js
@@ -1,55 +1,82 @@
-//returns an array of objects of the guesses the player has made so far
-/*
-  returns [
-            {
-              "meal" : $the_meal
-              "is_happy" : $true/false
-            }
-            {
-              "meal" : $the_meal
-              "is_happy" : $true/false
-            }
-        ]        
-*/
-
-var apple = {
-    type: "macintosh",
-    color: "red",
-    getInfo: function () {
-        return this.color + ' ' + this.type + ' apple';
-    }
-}
-var store = {  
   
-  startNewGame: function(rule) {
-    localStorage["zen_rule"] = rule;
-    localStorage["guesses"] = "";
-  },
+  //need increment and decrement for health bar
+  //3 guesses, and then he is full, so then the recipe button is highlighted. he can guess or not
   
-  getGuesses: function() {
-    if (localStorage["guesses"] != "") {      
-      return JSON.parse(localStorage["guesses"]);
-    } else {
-      return []
-    }
-  },
-
-  /*
-   takes the guess, and the boolean value for correctness of the guess meal
+  /*  
+  with javascript as you click things it changes the guess component div.  on "feed" it reads the data from the divs and then interprets it into the data store object
   */
-  addGuess: function(g, is_happy) {
-    if (localStorage["guesses"] != "") {    
-      var current_guesses = JSON.parse(localStorage["guesses"]);
-    } else {
-      current_guesses = []
-    } 
-    var guess = {
-      "meal" : g,
-      "is_happy" : is_happy
-    }  
-    current_guesses.push(guess);
-    localStorage["guesses"] = JSON.stringify(current_guesses);
-  
-  }
 
-}
+
+(function($){
+  
+  var Food = Backbone.Model.extend({
+    defaults: {
+      size: '',
+      food: ''
+    },
+  });
+  //a meal is an array of foods. the zen master's initial presentation is also a meal
+  var Meal = Backbone.Model.extend({
+    foods : [],
+    is_happy : false
+  });
+
+  var Guesses = Backbone.Collection.extend({
+      model: Meal
+    });
+  
+  // the view container for the guesses
+  var GuessView = Backbone.View.extend({
+    el: $('body'),
+    events: {
+      'click button#feed': 'addGuess'
+    },
+    initialize: function(){
+      _.bindAll(this, 'render', 'addGuess', 'appendGuess'); // remember: every function that uses 'this' as the current object should be in here
+
+      this.collection = new Guesses();
+      this.collection.bind('add', this.appendGuess); // collection event binder
+
+      this.render();      
+    
+    },
+  
+    render: function() {
+      var self = this;
+      $(this.el).append("<button id='feed'>Make guess/feed</button>");
+      //$('',this.el).append("<ul class='guesslist'></ul>");
+    },
+    addGuess: function() {
+      //will need to extract these foods from the html configuration
+      var meal = new Meal({
+        foods : [
+          new Food({size : "small", food : "nut"}),
+          new Food({size : "large", food : "nut"}),
+          new Food({size : "small", food : "fern"})
+          
+        ],
+        is_happy : Math.round(Math.random())
+        
+      })
+      this.collection.add(meal); // add item to collection; view is updated via event 'add'
+    },
+  
+    appendGuess: function(guess) {
+    
+      var html_str = "<li class='guess'><div class='rancherhead "+ (guess.get("is_happy") ? "happy" : "sad") + "'></div>";
+    
+      for (var i = 0; i < guess.get("foods").length; i++) {
+        var g = guess.get("foods")[i];
+        html_str += "<div class='foodtainer "+ g.get("size") + " "+ g.get("food")
+         + " '>" + g.get("size") + " "+ g.get("food") +  "</div>"
+      }
+      html_str +="</li>";
+      
+      $('.page ul.guesslist', this.el).append(html_str);
+    }
+  });
+  
+  
+  var guessView = new GuessView();
+  
+})(jQuery);
