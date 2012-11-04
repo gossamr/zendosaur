9458dff0155e062f3e69a782190e8cb7a20f3cd6
Karenism <karebearinmind@yahoo.com>
2012-11-04 12:33:10 -0800

    added view objects to datacore

--- js/datacore.js
+++ js/datacore.js
@@ -1,11 +1,3 @@
-  
-  //need increment and decrement for health bar
-  //3 guesses, and then he is full, so then the recipe button is highlighted. he can guess or not
-  
-  /*  
-  with javascript as you click things it changes the guess component div.  on "feed" it reads the data from the divs and then interprets it into the data store object
-  */
-
 
 (function($){
   
@@ -35,7 +27,7 @@
       _.bindAll(this, 'render', 'addGuess', 'appendGuess'); // remember: every function that uses 'this' as the current object should be in here
 
       this.collection = new Guesses();
-      this.collection.bind('add', this.appendGuess); // collection event binder
+      this.collection.bind('add', this.appendGuess); 
 
       this.render();      
     
@@ -44,7 +36,8 @@
     render: function() {
       var self = this;
       $(this.el).append("<button id='feed'>Make guess/feed</button>");
-      //$('',this.el).append("<ul class='guesslist'></ul>");
+      $('.page', this.el).append("<button>uheot</button>")
+      
     },
     addGuess: function() {
       //will need to extract these foods from the html configuration
@@ -71,12 +64,67 @@
          + " '>" + g.get("size") + " "+ g.get("food") +  "</div>"
       }
       html_str +="</li>";
-      
-      $('.page ul.guesslist', this.el).append(html_str);
+      console.log("the element")
+      console.log($( this.el));
+      $(".dinospot", this.el).append(html_str);
     }
   });
   
   
+  var DinoView = Backbone.View.extend({
+     el: $('#zen_rule'),
+     events: {
+     },
+     initialize: function(){
+        _.bindAll(this, 'render'); 
+       this.render()
+     },
+
+     render: function() {
+       var self = this;
+       
+       //generate a zen master rule for this dinosaur
+       var map_size = [0,'small','medium','large'];
+       var color = [0,'red','green','yellow'];  
+
+       var zen_rule = rndrule();
+       //meal is an array with food objects that have size property and color property
+       var zen_meal = mealgen(zen_rule)
+       var foods_array = [];
+       console.log(zen_meal)
+       for(var i = 0; i < zen_meal.length; i++) {
+         var food = zen_meal[i];
+         
+         foods_array.push(new Food({size : map_size[food.size], food : color[food.color]}));
+         
+       }
+       console.log(foods_array)
+       var meal = new Meal({
+         foods : foods_array,
+         is_happy : true
+       });
+       
+       var html_str = "<div><ul><li>";
+       
+       for(var i = 0; i < meal.get("foods").length; i++) {
+         html_str += "<div class='foodtainer " + meal.get("foods")[i].get("food") + " "+ meal.get("foods")[i].get("size") +"'></div>"
+       }
+       html_str += "</li></ul></div>";
+       
+       console.log(html_str);
+       console.log($(this.el));
+       $(this.el).append(html_str);
+       
+       console.log($(this.el).html());
+     },
+
+   });
+  
+  
+  
   var guessView = new GuessView();
+  var dinoView = new DinoView();
+  
+  
   
 })(jQuery);
