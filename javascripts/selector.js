5d4e7cdd4b22904602cc37010caee313bb0c5197
Jehan Tremback <jehan.tremback@gmail.com>
2012-11-04 10:28:19 -0800

    basic selector coded

---
+++ javascripts/selector.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("#keypad .palette .foodtainer").click(function(){
+      $("#keypad .palette .foodtainer").removeClass("selected");
+      $(this).addClass("selected");
+  });
+  $("#keypad .grid .foodtainer").click(function(){
+    var selected = $("#keypad .palette .foodtainer.selected");
+    var thisfood = $(this).attr("data-food");
+    var thisimage = $(this).html();
+    selected.data(thisfood);
+    selected.html(thisimage);
+  })
+});
